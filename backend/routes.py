c7eab4216a258c8270de22e584e20d3a3cd0b47b
HossamJa <jamhosam123@gmail.com>
2025-08-13 18:09:16 -0400

    implemented pictures service by adding REST API

--- backend/routes.py
+++ backend/routes.py
@@ -33,39 +33,64 @@ def count():
 ######################################################################
 # GET ALL PICTURES
 ######################################################################
+
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    """Return all the pictures"""
+    return jsonify(data), 200
 
 ######################################################################
 # GET A PICTURE
 ######################################################################
 
-
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    """Return a picture by ID"""
+    for picture in data:
+        if picture["id"] == id:
+            return jsonify(picture), 200
+    abort(404, f"Picture with ID {id} was not found!")
 
 
 ######################################################################
 # CREATE A PICTURE
 ######################################################################
+
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    """Create new picture"""
+    picture_data = request.json
+    for pic in data:
+        if pic["id"] == picture_data["id"]:
+            return {"Message": f"picture with id {picture_data['id']} already present"}, 302
+    
+    data.append(picture_data)
+    return picture_data, 201
 
 ######################################################################
 # UPDATE A PICTURE
 ######################################################################
 
-
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    """ Update an existing picture"""
+    for index, pic in enumerate(data):
+        if pic["id"] == id:
+            data[index] = request.json
+            return pic, 201
+
+    abort(404, f"Picture with ID {id} was not found!")
+
 
 ######################################################################
 # DELETE A PICTURE
 ######################################################################
 @app.route("/picture/<int:id>", methods=["DELETE"])
 def delete_picture(id):
-    pass
+    """ Delete an existing picture"""
+    for index, pic in enumerate(data):
+        if pic["id"] == id:
+            del(data[index])
+            return "", 204
+
+    abort(404, f"Picture with ID {id} was not found!")
